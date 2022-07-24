6741817e396c596aefd5e967e253b0d63506912a
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2022-07-25 02:54:30 +0530

    _offline file changes

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,7 +1,7 @@
-import Link from 'next/image'
+import Link from 'next/link'
 
 export default function Offline() {
     return (
-        <>App is Offline Please contact <Link href='tel:+91-7875995888' passHref><a>@7875995888</a></Link>.</>
+        <div className="flex flex-col justify-center items-center text-center min-h-screen">App is Offline Please contact <Link href='tel:+91-7875995888' passHref><a>@7875995888</a></Link>.</div>
     )
 }
